be73aa87a954e0f09fbceda48989ee7280fa5faf
abhijeet.shende <abhijeet.shende12@gmail.com>
2020-02-06 14:04:56 +0530

    landing page is now having tables

--- child_views/landing_page/landing_page.js
+++ child_views/landing_page/landing_page.js
@@ -1,3 +1,11 @@
-app.controller('landingPageCtrl', ['$scope', function ($scope) {
+app.controller('landingPageCtrl', ['$scope', 'sidebarService', function ($scope, sidebarService) {
     console.log("landingpage");
+    $scope.dgroom_data;
+    $scope.ltroom_data;
+    sidebarService.getSidebarInfo()
+        .then(function (res) {
+            $scope.dgroom_data = res.data.H1[0].H2[0].H3[0];
+            $scope.ltroom_data = res.data.H1[0].H2[0].H3[1];
+
+        });
 }])
